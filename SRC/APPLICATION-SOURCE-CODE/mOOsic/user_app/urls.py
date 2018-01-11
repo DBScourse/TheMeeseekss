37e9f567124985553fd84cc1483eb7c2341fb96d
gilikatabi <gilikatabi@gmail.com>
2018-01-11 19:42:00 +0200

    remove un-needed index view

--- SRC/APPLICATION-SOURCE-CODE/mOOsic/user_app/urls.py
+++ SRC/APPLICATION-SOURCE-CODE/mOOsic/user_app/urls.py
@@ -3,7 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^index', views.index, name="index"),
     url(r'^get_playlists', views.get_playlists, name="get_playlists")
 ]
--- SRC/APPLICATION-SOURCE-CODE/mOOsic/user_app/views.py
+++ SRC/APPLICATION-SOURCE-CODE/mOOsic/user_app/views.py
@@ -4,19 +4,6 @@ from django.http import JsonResponse
 import db_handler as dbhandler
 
 
-from django.http import HttpResponse
-from django.template import loader
-
-
-def index(request):
-    bala = dbhandler.sample_sql_query()
-    template = loader.get_template('user_app/index.html')
-    context = {'bala': bala}
-    return HttpResponse(template.render(context, request))
-
-
-
-
 def get_playlists(request):
     bala = dbhandler.sample_sql_query()
     response = [{'name': 'try', 'id': 1}, {'name': 'if', 'id': 2}, {'name': 'works', 'id': 3}]
