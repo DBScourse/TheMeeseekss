0116bcb2962a64f392ae919f14a464b0d61eeddc
yotam ronen <yotronen@gmail.com>
2018-01-11 20:08:47 +0200

    added the original database settings, uncomment the other 'default' when we use the TAU DB

--- SRC/APPLICATION-SOURCE-CODE/mOOsic/mOOsic/settings.py
+++ SRC/APPLICATION-SOURCE-CODE/mOOsic/mOOsic/settings.py
@@ -75,13 +75,17 @@ WSGI_APPLICATION = 'mOOsic.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'DbMysql19',
+    #     'USER': 'DbMysql19',
+    #     'PASSWORD': 'DbMysql19',
+    #     'HOST': 'localhost',
+    #     'PORT': '3305',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DbMysql19',
-        'USER': 'DbMysql19',
-        'PASSWORD': 'DbMysql19',
-        'HOST': 'localhost',
-        'PORT': '3305',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
