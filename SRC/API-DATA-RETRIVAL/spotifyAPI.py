aa7f1ee341d606cd5440add9e3404a3774ec644e
yotam ronen <yotronen@gmail.com>
2018-01-13 11:23:15 +0200

    spotify

--- SRC/API-DATA-RETRIVAL/spotifyAPI.py
+++ SRC/API-DATA-RETRIVAL/spotifyAPI.py
@@ -16,58 +16,61 @@ def spotify_trackid(query):
     except IndexError:
         print('Song not found in search:' + query)
         return None
-    except spotipy.client.SpotifyException:
+    except spotipy.client.SpotifyException as err:
+        print(err)
         print('Spotify exception occurred. going to sleep and trying again.')
         time.sleep(30)
         return spotify_trackid(query)
-    except ConnectionError:
+    except ConnectionError as err:
+        print(err)
         print('Connection Error occurred. going to sleep and trying again.')
         time.sleep(30)
         return spotify_trackid(query)
-    except json.ValueError:
-        print('Faulty json - probably the response from spotify is bad')
-        print('Skipping this song: ' + query)
+    except:
+        print('An error has occurred. ignoring song')
         return None
 
 
-def spotify_track_mood(track_ids):
+def spotify_track_mood(query):
     client_credentials_manager = SpotifyClientCredentials(client_id="a30fb9dcef504d679a32798ee598de58",
                                                           client_secret="58723610684241ecaffe055cd9a72f0a")
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     try:
-        result = sp.audio_features(track_ids)
+        result = sp.audio_features(query)
         return result
-    except spotipy.client.SpotifyException:
-        print('Spotify exception occurred')
-        exit(1)
-    except ConnectionError:
+    except spotipy.client.SpotifyException as err:
+        print(err)
+        print('Spotify exception occurred. going to sleep and trying again.')
+        time.sleep(30)
+        return spotify_track_mood(query)
+    except ConnectionError as err:
+        print(err)
         print('Connection Error occurred. going to sleep and trying again.')
         time.sleep(30)
-        return spotify_track_mood(track_ids)
+        return spotify_track_mood(query)
+    except ValueError as err:
+        print(err)
+        print('Value Error occurred. going to sleep and trying again.')
+        time.sleep(30)
+        return spotify_track_mood(query)
 
-
-ids = []
-f = open('lyrics_list.json')
-d = json.load(f)
-for i in range(len(d)):
-    s = d[i]['track'] + ' ' + d[i]['artist']
-    track_id = spotify_trackid(s)
-    if track_id is not None:
-        d[i]['id'] = track_id
-        ids.append(track_id)
 
 track_dict = {}
-for track in d:
-    track_dict[track['id']] = track
+source = open('lyrics_list.json')
+target = open('spotifyData.json', 'w')
+data = json.load(source)
+for i in range(len(data)):
+    q = str(data[i]['track'] + ' ' + data[i]['artist'])
+    track_id = spotify_trackid(q)
+    if track_id is not None and track_id not in track_dict:
+        res = spotify_track_mood(str(track_id))
+        if res is not None:
+            data[i]['danceability'] = res[0]['danceability']
+            data[i]['energy'] = res[0]['energy']
+            data[i]['id'] = track_id
+            track_dict[track_id] = data[i]
+            print('#################' + q + '#################')
+            target.truncate()
+            target.write(json.dumps(track_dict))
 
-print('############## FINISHED GETTING SONG IDS##############')
-time.sleep(30)
-subseq50 = [ids[i:i + 50] for i in range(0, len(ids), 50)]
-for seq in subseq50:
-    moods50 = spotify_track_mood(seq)
-    for feats in moods50:
-        track_dict[feats['id']]['danceability'] = feats['danceability']
-        track_dict[feats['id']]['energy'] = feats['energy']
 
-target = open('spotifyData.json', 'a')  # TODO
-target.write(json.dumps(track_dict))
