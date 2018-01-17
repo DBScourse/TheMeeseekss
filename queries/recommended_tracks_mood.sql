7e4ea493bb2601293a00afc9e2de3c109621d876
= <=>
2018-01-17 23:04:11 +0200

    fixed search by mood only

--- queries/recommended_tracks_mood.sql
+++ queries/recommended_tracks_mood.sql
@@ -1,9 +1,14 @@
-SELECT *
-FROM Tracks_tbl 
+SELECT tb.track_id,
+    track_name,
+    album_name,
+    artist_name 
+FROM Tracks_tbl AS tb
+JOIN Artists_tbl
+ON tb.artist_id = Artists_tbl.artist_id
 WHERE mood_id = 
 (
 	SELECT mood_id
     FROM Moods_tbl
-    WHERE danceability = {danceability} AND energy = {energy}
+    WHERE ABS(danceability - {danceability}) < 0.0001 AND ABS(energy - {danceability}) < 0.0001
 )
 LIMIT 20
