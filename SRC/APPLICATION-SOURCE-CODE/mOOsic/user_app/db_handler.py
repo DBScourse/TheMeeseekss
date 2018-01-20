110cdab434a203383fc704a72300369f658bd293
yotam ronen <yotronen@gmail.com>
2018-01-20 21:28:39 +0200

    changed recommendation accuracy to 0.01

--- SRC/APPLICATION-SOURCE-CODE/mOOsic/user_app/db_handler.py
+++ SRC/APPLICATION-SOURCE-CODE/mOOsic/user_app/db_handler.py
@@ -123,11 +123,11 @@ def create_playlist(username, danceability, energy, playlist_name, tag=None):
 
         if tag is not None:
             q = (
-                "SELECT tb.track_id, track_name, album_name, tb.artist_id, artist_name FROM Tracks_tbl AS tb JOIN TracksToTags_tbl AS ttb ON tb.track_id = ttb.track_id JOIN Artists_tbl ON tb.artist_id = Artists_tbl.artist_id WHERE tag_id = (SELECT tag_id FROM Tags_tbl WHERE tag_name = %s) AND mood_id = (SELECT mood_id FROM Moods_tbl WHERE ABS(danceability - %s) < 0.0001 AND ABS(energy - %s) < 0.0001 LIMIT 1) LIMIT 20")
+                "SELECT tb.track_id, track_name, album_name, tb.artist_id, artist_name FROM Tracks_tbl AS tb JOIN TracksToTags_tbl AS ttb ON tb.track_id = ttb.track_id JOIN Artists_tbl ON tb.artist_id = Artists_tbl.artist_id WHERE tag_id = (SELECT tag_id FROM Tags_tbl WHERE tag_name = %s) AND mood_id = (SELECT mood_id FROM Moods_tbl WHERE ABS(danceability - %s) < 0.01 AND ABS(energy - %s) < 0.01 LIMIT 1) LIMIT 20")
             cursor.execute(q, (tag, danceability, energy))
         else:
             q = (
-                "SELECT tb.track_id, track_name, album_name, tb.artist_id, artist_name FROM Tracks_tbl AS tb JOIN Artists_tbl ON tb.artist_id = Artists_tbl.artist_id WHERE mood_id = (SELECT mood_id FROM Moods_tbl WHERE ABS(danceability - %s) < 0.0001 AND ABS(energy - %s) < 0.0001 LIMIT 1) LIMIT 20")
+                "SELECT tb.track_id, track_name, album_name, tb.artist_id, artist_name FROM Tracks_tbl AS tb JOIN Artists_tbl ON tb.artist_id = Artists_tbl.artist_id WHERE mood_id = (SELECT mood_id FROM Moods_tbl WHERE ABS(danceability - %s) < 0.01 AND ABS(energy - %s) < 0.01 LIMIT 1) LIMIT 20")
             cursor.execute(q, (danceability, energy))
 
         results = [(track_id, track_name, album_name, artist_id, artist_name) for
