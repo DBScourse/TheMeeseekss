218d28f25f455ae164fed017b10d1f0fd2ef1db7
yotam ronen <yotronen@gmail.com>
2018-01-21 17:12:03 +0200

    for Adi

--- queries/recommended_tracks_mood.sql
+++ queries/recommended_tracks_mood.sql
@@ -1,4 +1,4 @@
-SELECT tb.track_id,
+SELECT tb.track_id, mood_id,
     track_name,
     album_name,
     tb.artist_id,
@@ -6,11 +6,11 @@ SELECT tb.track_id,
 FROM Tracks_tbl AS tb
 JOIN Artists_tbl
 ON tb.artist_id = Artists_tbl.artist_id
-WHERE mood_id = 
+WHERE mood_id IN
 (
     SELECT mood_id
     FROM Moods_tbl
-    WHERE ABS(danceability - {danceability}) < 0.0001 AND ABS(energy - {energy}) < 0.0001
-    LIMIT 1
+    WHERE ABS(danceability - 0.118) < 0.1 AND ABS(energy - 0.221) < 0.1
+    order by ABS(danceability - 0.118) + ABS(energy - 0.221) ASC
 )
 LIMIT 20
